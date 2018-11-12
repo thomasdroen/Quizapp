4354d39ddba7ecf9b484436b229b64392e1ba7fc
LAPTOP-LO9VFG89\Thomas <thoma.sd@live.no>
2018-11-12 17:52:18 +0100

    Added getting categories from server

--- app/src/main/java/no/ntnu/stud/erikfossum/quizapp/MainActivity.java
+++ app/src/main/java/no/ntnu/stud/erikfossum/quizapp/MainActivity.java
@@ -5,26 +5,82 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    List<String> input = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler.post(runnableCode);
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        /*for (int i = 0; i < 100; i++) {
             input.add("Test" + i);
-        }// define an adapter
+        }// define an adapter*/
         mAdapter = new MainAdapter(input);
         recyclerView.setAdapter(mAdapter);
+
+
+
     }
+
+    Handler handler = new Handler();
+    private RequestQueue queue;
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+            if(queue == null){
+                queue = Volley.newRequestQueue(MainActivity.this);
+            }
+            String url ="http://10.0.2.2:8080/QuizServer/api/quiz/getCategories";
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                input.clear();
+                                JSONArray array = new JSONArray(response);
+                                for (int i = 0; i < array.length(); i++) {
+                                    JSONObject object = array.optJSONObject(i);
+                                    String line = object.optString("categoryName");
+                                    input.add(line);
+                                }
+
+                                mAdapter.notifyDataSetChanged();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            });
+            queue.add(stringRequest);
+
+            // Repeat this the same runnable code block again another 2 seconds
+            handler.postDelayed(runnableCode, 2000);
+        }
+    };
 }
--- app/src/main/java/no/ntnu/stud/erikfossum/quizapp/QuizActivity.java
+++ app/src/main/java/no/ntnu/stud/erikfossum/quizapp/QuizActivity.java
@@ -2,12 +2,132 @@ package no.ntnu.stud.erikfossum.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+
+    private TextView mQuestionView;
+    private Button mButtonChoice1;
+    private Button mButtonChoice2;
+    private Button mButtonChoice3;
+    private Button mButtonChoice4;
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionView = (TextView)findViewById(R.id.question);
+        mButtonChoice1 = (Button)findViewById(R.id.choice1);
+        mButtonChoice2 = (Button)findViewById(R.id.choice2);
+        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+        mButtonChoice4 = (Button)findViewById(R.id.choice4);
+
+        mButtonChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //My logic for Button goes in here
+/*
+                if (mButtonChoice1.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+
+        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice2.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+
+
+
+        mButtonChoice3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice3.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+
+        mButtonChoice4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice3.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });*/
+            }
+/*
+    public void updateQuestion(){
+        mQuestionView.setText(mQuestionLibrary.getQuestion(mQuestionNumber));
+        mButtonChoice1.setText(mQuestionLibrary.getChoice1(mQuestionNumber));
+        mButtonChoice2.setText(mQuestionLibrary.getChoice2(mQuestionNumber));
+        mButtonChoice3.setText(mQuestionLibrary.getChoice3(mQuestionNumber));
+
+        mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
+        mQuestionNumber++;
     }
+    private void updateScore(int point) {
+        mScoreView.setText("" + mScore);
+    }
+*/
+            })
+    ;}
 }
+
+
